4a633a25412af6aa4974a54a6b411d4865affb5a
kimjunsung04 <koreatank3@gmail.com>
2021-06-25 11:16:58 +0900

    테스트 print 삭제, 코드 스타일 black 변경

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ from oauth2client.tools import argparser
 url = input("다운받을 유뷰브 링크 or 영상제목을 입력해주세요 : ")
 
 if "http" in url:
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    ydl = youtube_dl.YoutubeDL({"outtmpl": "%(id)s%(ext)s"})
     ydl.add_default_info_extractors()
     result = ydl.extract_info(url, download=False)
 else:
-    videosSearch = VideosSearch(f'{url}', limit = 1)
+    videosSearch = VideosSearch(f"{url}", limit=1)
     ytde = videosSearch.result()
-    y_id = ytde['result'][0]['id']
+    y_id = ytde["result"][0]["id"]
     url = f"https://www.youtube.com/watch?v={y_id}"
 
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    ydl = youtube_dl.YoutubeDL({"outtmpl": "%(id)s%(ext)s"})
     ydl.add_default_info_extractors()
     result = ydl.extract_info(url, download=False)
 
 m_url = result
-print(m_url)
-title = m_url['title']
-m_url = m_url['formats'][1]['url']
+title = m_url["title"]
+m_url = m_url["formats"][1]["url"]
 file = wget.download(m_url, f"./{title}.mp3")
